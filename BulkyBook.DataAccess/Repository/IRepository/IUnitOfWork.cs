0accc5245670a5f194e5ce12c55cb69a21df225e
Stasgavriluk <Stasgavrilyk22@gmail.com>
2023-01-20 14:22:14 +0200

    Add half part of the project

---
+++ BulkyBook.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace BulkyBook.DataAccess.Repository.IRepository
+{
+	public interface IUnitOfWork
+	{
+		ICategoryRepository Category { get; }
+
+		ICoverTypeRepository CoverType { get; }
+
+        IProductRepository Product { get; }
+        void Save();
+	}
+}
+
