64d92e09ba71a470f7f8f0ded56f759c912f4ed2
Stasgavriluk <Stasgavrilyk22@gmail.com>
2023-02-03 20:15:57 +0200

    Loading Order Details Header

---
+++ BulkyBook.Models/ViewModels/OrderVM.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace BulkyBook.Models.ViewModels
+{
+	public class OrderVM
+	{
+		public OrderHeader OrderHeader { get; set; }
+
+		public IEnumerable<OrderDetails> OrderDetails { get; set; }
+	}
+}
+
